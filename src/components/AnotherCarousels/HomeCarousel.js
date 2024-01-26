3bcfb6eb13c426e0b8976862088cffcdf240f5a2
Snehajain28 <snehajain28022004@gmail.com>
2024-01-26 18:44:11 +0530

    handle error

--- src/components/AnotherCarousels/HomeCarousel.js
+++ src/components/AnotherCarousels/HomeCarousel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import Card from '../Card/Card.js';
-i
+
 
 const HomeCarousel = ({ CarouselData, sectionName }) => {
 
