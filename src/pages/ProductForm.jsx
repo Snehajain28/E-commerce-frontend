2705ce6f582ec7ce3ba0ef1e6f5b2955c195ffad
Snehajain28 <132094406+Snehajain28@users.noreply.github.com>
2024-01-28 02:00:15 +0530

    Update ProductForm.jsx

--- src/pages/ProductForm.jsx
+++ src/pages/ProductForm.jsx
@@ -34,7 +34,7 @@ const ProductForm = () => {
         const res = await axios.post('http://localhost:5000/api/v1/product/add-product', formData, {
             headers: { "Content-type": "multipart/form-data" },
         });
-
+console.log(res)
     }
 
     return (
